66879c55479e7a7a4ec9a2ca2dd6f9b2e296f49e
jamshidakramovich14 <jamshidturdiyev103>
2024-01-19 08:04:05 +0500

    all files

---
+++ script.js
@@ -0,0 +1,58 @@
+"use strict";
+1 - masala;
+
+// function calc(n){
+//     let pow = n ** 3;
+//     console.log(pow);
+// }
+
+// let d = calc(5);
+
+// 2- masala
+
+// function plus(a, b){
+//     let yigindi = a + b;
+//     console.log(yigindi)
+// }
+// let d = plus(3, 8)
+
+// 3-masala
+
+// function plus(a, b) {
+//   let yigindi = a - b;
+//   console.log(yigindi);
+// }
+// let d = plus(3, 8);
+
+// 4-masala
+
+// function sonDaraja(n) {
+//     console.log(n**2);
+//     console.log(n**3);
+//     console.log(n**4);
+// }
+
+// sonDaraja(2);
+
+// 5- masala
+
+// function calc(n){
+//     for(let i = 1; i <= n; i++){
+//         if(i % 2 == 1){
+//             console.log(i)
+//         }
+//     }
+// }
+
+// let b = calc(60)
+
+// 6- masala
+// function calc(n) {
+//   for (let i = 1; i <= n; i++) {
+//     if (i % 2 == 0) {
+//       console.log(i);
+//     }
+//   }
+// }
+
+// let b = calc(50)
